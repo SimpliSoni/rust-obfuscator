4fb94d808bc687677259e21cf6aab54afd221715
karan-soni-dotcom <SimpliSoni@users.noreply.github.com>
2025-08-17 03:54:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
